bd8e0cdf0c5c24014c02cd68fdca918544988cdb
987456987 <donufrak112697@gmail.com>
2023-09-13 15:16:19 -0700

    start end slider

--- vid2gif/.eslintrc.cjs
+++ vid2gif/.eslintrc.cjs
@@ -17,6 +17,6 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
-    'plugin:prettier/recommended'
+    // 'plugin:prettier/recommended'
   ]
 }
--- vid2gif/src/renderer/src/components/VideoPlayer.jsx
+++ vid2gif/src/renderer/src/components/VideoPlayer.jsx
@@ -1,8 +1,22 @@
-/* eslint-disable prettier/prettier */
-import { useRef } from 'react'
+import { useRef, useState, useEffect } from 'react'
+import Slider from 'rc-slider'
+import 'rc-slider/assets/index.css'
+import '../assets/rc-custom.css'
 
 const VideoPlayer = () => {
   const videoRef = useRef(null)
+  const [startTime, setStartTime] = useState(0)
+  const [endTime, setEndTime] = useState(0)
+  const [duration, setDuration] = useState(0)
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.addEventListener('loadedmetadata', () => {
+        setDuration(videoRef.current.duration)
+        setEndTime(videoRef.current.duration)
+      })
+    }
+  }, [])
 
   const handleFileSelect = (e) => {
     const file = e.target.files[0]
@@ -14,10 +28,58 @@ const VideoPlayer = () => {
     }
   }
 
+  const handleSliderChange = (values) => {
+    setStartTime(values[0])
+    setEndTime(values[1])
+  }
+
+  const handlePlayRange = () => {
+    if (videoRef.current) {
+      videoRef.current.currentTime = startTime
+      videoRef.current.play()
+    }
+  }
+
+  useEffect(() => {
+    const handleTimeUpdate = () => {
+      if (videoRef.current) {
+        if (videoRef.current.currentTime >= endTime) {
+          videoRef.current.pause()
+        }
+      }
+    }
+
+    videoRef.current.addEventListener('timeupdate', handleTimeUpdate)
+
+    return () => {
+      videoRef.current.removeEventListener('timeupdate', handleTimeUpdate)
+    }
+  }, [endTime])
+
   return (
-    <div>
+    <div className='outer-container'>
       <input type="file" accept="video/*" onChange={handleFileSelect} />
-      <video ref={videoRef} controls style={{ width: '100%', height: 'auto' }} />
+
+      <video ref={videoRef} controls style={{ width: '100%', height: 'auto' }}>
+        <source src="" type="video/mp4" />
+      </video>
+
+      <div style={{ textAlign: 'center', marginTop: '10px' }}>
+        <span>Start Time: {startTime.toFixed(1)}</span>
+        <span style={{ marginLeft: '10px', marginRight: '10px' }}>|</span>
+        <span>End Time: {endTime.toFixed(1)}</span>
+      </div>
+
+      <Slider
+        range
+        min={0}
+        max={duration}
+        step={0.1}
+        value={[startTime, endTime]}
+        onChange={handleSliderChange}
+      />
+
+      <button onClick={handlePlayRange}>Play Range</button>
     </div>
   )
 }
